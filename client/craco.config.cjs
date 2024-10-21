a1ef3d9dc8b20a1ddbd36b3b85a055d7b6a37aad
Jithendra <kanamarlapudi.jithendra@gmail.com>
2024-10-21 14:06:03 +0530

    updated package.json

--- client/craco.config.cjs
+++ client/craco.config.cjs
@@ -1,16 +1,22 @@
 // craco.config.cjs
 
 module.exports = {
-    webpack: {
-      configure: (webpackConfig, { env, paths }) => {
-        // Add ts-loader
-        webpackConfig.module.rules.push({
-          test: /\.tsx?$/,
-          use: "ts-loader",
-          exclude: /node_modules/,
-        });
-        return webpackConfig;
-      },
+  devServer: (devServerConfig) => {
+    return {
+      ...devServerConfig,
+      allowedHosts: ['localhost'], // Or 'all' to allow all hosts
+    };
+  },
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      // Add ts-loader
+      webpackConfig.module.rules.push({
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      });
+      return webpackConfig;
     },
-  };
+  },
+};
    
