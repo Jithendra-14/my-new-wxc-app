e4e5502e660b661c221e86cf1eb0740dc76f439f
Jithendra <kanamarlapudi.jithendra@gmail.com>
2024-10-22 07:10:24 +0530

    updated the newsletter aug 21

--- client/public/web/de/newsletter/denlaug21/index.js
+++ client/public/web/de/newsletter/denlaug21/index.js
@@ -1,46 +1,17 @@
-// https://az2024htmlpages.blob.core.windows.net/data/web/de/newsletter/denlaug21/img/acc_msb.png
-
 $(document).ready(function() {
-  fetchSASTokenAndInit();
+  initializePageWithAPIToBlob();
 });
 
-async function getSASToken() {
-  try {
-    const response = await fetch("/api/azure/getSAS");
-    const data = await response.json();
-    return data.token;
-  } catch (error) {
-    console.error("Error fetching SAS token:", error);
-    return ""; // Return empty string or handle error accordingly
-  }
-}
-
-// Function to fetch SAS token and initialize the page
-function fetchSASTokenAndInit() {
-  getSASToken()
-    .then((token) => {
-      // Once the SAS token is fetched, call the function to generate the HTML page
-      initializePageWithSASToken(token);
-    })
-    .catch((error) => {
-      console.error("Error fetching SAS token:", error);
-      initializePageWithSASToken(""); // Call init with an empty token in case of failure
-    });
-}
-
 // Example function that initializes the page using the SAS token
-function initializePageWithSASToken(sasToken) {
-  const image_prefix =
-    "https://az2024htmlpages.blob.core.windows.net/data/web/de/newsletter/denlaug21/";
-  const image_suffix = "?" + sasToken;
+function initializePageWithAPIToBlob() {
+  const image_prefix = "/api/web/de/newsletter/denlaug21/";
 
   $("img").each(function() {
     // Get the current src of the image
     const currentSrc = $(this).attr("src");
 
     // Update the src attribute with the new value
-    const newSrc = image_prefix + currentSrc + image_suffix;
-    console.log(newSrc);
+    const newSrc = image_prefix + currentSrc;
     $(this).attr("src", newSrc);
   });
 }
